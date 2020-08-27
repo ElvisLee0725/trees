4250fd68b50689a4c01b020772d0c64735dd8b11
ElvisLee <elvislee0725@gmail.com>
2020-08-26 23:29:19 -0700

    Add a solution using prev treenode

--- ValidateBinarySearchTree/src/Solution.java
+++ ValidateBinarySearchTree/src/Solution.java
@@ -1,8 +1,14 @@
+// Method 1:
 // Use a helper function with min and max value
 // At each node, check if min and max are not null and if current node's val is smaller or greater than it?
 // If so, return false since it is not a BST
 // Recurse to the left and right child and set root.val to min and max
+// Time: O(n), Space: O(logn)
 
+// Method 2:
+// Use a prev TreeNode to record the current node. In-order traverse and check if cur node's value is smaller or
+// equal to prev's val. If so, return false
+// Return the result from left and right child
 // Time: O(n), Space: O(logn)
 
 public class Solution {
@@ -17,7 +23,7 @@ public class Solution {
         b.left = c;
         b.right = d;
 
-        System.out.print(new Solution().isValidBST(root));
+        System.out.print(new Solution().isValidBSTInOrder(root));
     }
 
     public boolean isValidBST(TreeNode root) {
@@ -33,5 +39,23 @@ public class Solution {
         }
 
         return helper(root.left, min, root.val) && helper(root.right, root.val, max);
+    }
+
+    TreeNode prev = null;
+    public boolean isValidBSTInOrder(TreeNode root) {
+        if(root == null) {
+            return true;
+        }
+
+        boolean left = isValidBSTInOrder(root.left);
+
+        if(prev != null && root.val <= prev.val) {
+            return false;
+        }
+        prev = root;
+
+        boolean right = isValidBSTInOrder(root.right);
+
+        return left && right;
     }
 }
